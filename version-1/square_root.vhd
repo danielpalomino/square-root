3e39807cc20c96888fc4152b91cd846ba731d8ab
Daniel Palomino <danielmpalomino@gmail.com>
2011-09-21 12:19:45 -0300

    iniciando versão 1

---
+++ version-1/square_root.vhd
@@ -0,0 +1,21 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY square_root IS
+	GENERIC (n: INTEGER:=8)
+	PORT(
+		clk, reset: 	IN STD_LOGIC;
+		n: 			IN STD_LOGIC_VECTOR(n-1 DOWNTO 0)
+	);
+
+END square_root;
+
+ARCHITECTURE behavioral OF square_root IS
+
+BEGIN
+
+PROCESS
+	
+	
+END behavioral;
