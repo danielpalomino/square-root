a768fcc6dd703b21ec24dea30a29a7a981493a82
Daniel Palomino <danielmpalomino@gmail.com>
2011-09-27 14:18:00 -0300

    versão 1 descrita e testada

--- version-1/square_root.vhd
+++ version-1/square_root.vhd
@@ -6,11 +6,11 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 ENTITY square_root IS
 	GENERIC (n: INTEGER:=8);
 	PORT(
-		clk, reset: 	IN STD_LOGIC;
+		clk, reset, start: 	IN STD_LOGIC;
 		number:	IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
 		root:		OUT STD_LOGIC_VECTOR((n-1)/2 DOWNTO 0)
 	);
-
+	
 END square_root;
 
 ARCHITECTURE behavioral OF square_root IS
@@ -24,21 +24,25 @@ BEGIN
 	IF reset = '1' THEN
 		root <= (OTHERS=>'0');
 	ELSIF clk'EVENT AND clk='1' THEN
-		r:=1;
-		d:=2;
-		s:=4;
-		t:=1;
-		WHILE t=1 LOOP
-			r:=r+1;
-			d:=d+2;
-			s:=s+d+1;
-			IF s > conv_integer(number) THEN
-				t:=0;
-			ELSE
-				t:=1;
+		IF start = '1' THEN
+			r:=1;
+			d:=2;
+			s:=4;
+			t:=1;
+		ELSE
+			IF t=1 THEN
+				r:=r+1;
+				d:=d+2;
+				s:=s+d+1;
+				IF s > conv_integer(number) THEN
+					t:=0;
+					root<= conv_std_logic_vector(r,(n/2));
+				ELSE
+					t:=1;
+				END IF;
 			END IF;
-		END LOOP;
-		root<= conv_std_logic_vector(r,(n/2));
+		END IF;
+		
 	END IF;
 
 END PROCESS;
